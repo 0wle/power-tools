5108b02a20ac055b74bec11e9bf81992520e4e1c
hagenahm <michel.hagenah@gmail.com>
2022-06-23 10:12:46 +0200

    added message if no deletions occured

--- git-clean-simple/git_clean.sh
+++ git-clean-simple/git_clean.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-didSomething=("false")
+#TODO boolean
+#TODO flags für logs oder nicht
+#TODO flags für quiet
+#TODO filepath mit datum
 
+didSomething=false
 for DIR in */; do
 cd $DIR 
 output=$(git clean -x -f)
 if [ "$output" != "" ]; then
-    didSomething=("true")
+    didSomething=true
     echo "$output" >> ../log.txt
 fi 
 cd ..; done
 
-if [ "$didSomething" == "false" ]; then
+if [ "$didSomething" = false ]; then
     echo "No files were deleted!"
 fi
