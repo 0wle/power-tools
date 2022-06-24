9bab8ad6114e1dfff7df2befcc48186f0e56cc08
hagenahm <michel.hagenah@gmail.com>
2022-06-24 09:29:34 +0200

    added absolute path

--- git-clean-simple/git_clean.sh
+++ git-clean-simple/git_clean.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #TODO flags für logs oder nicht
+#TODO flag für filepath specification
 #TODO flags für quiet
 #TODO dry run functionality with logging
 
@@ -8,11 +9,11 @@ function delete {
     output=$(git clean -x -f);
     if [ "$output" != "" ]; then
         if [ "$1" = false ]; then
-            echo "TIME: $(date +%H-%M-%S)" >> ".$2";
+            echo "TIME: $(date +%H-%M-%S)" >> "$2";
             didSomething=true;
         fi
         if [ "$3" = true ]; then
-            echo "$output" >> ".$2";
+            echo "$output" >> "$2";
         fi
         if [ "$4" = false ]; then
             echo "$output";
@@ -32,7 +33,17 @@ function checkForNoDeletions {
     fi
 }
 
-filepath="./$(date +%d-%m-%y)_clean-log.txt"
+specifiedLocation=false
+filepath=""
+if [ specifiedLocation = false ];
+then
+    filepath=$(pwd);
+else
+    # exchange this for the arg value
+    filepath=$(pwd);
+fi
+
+filepath="$filepath/$(date +%d-%m-%y)_clean-log.txt"
 didSomething=false
 shouldLog=true
 isQuiet=false
