e0fa7f02e54d4917dfc55a0eeeee9823316d8b70
hagenahm <michel.hagenah@gmail.com>
2022-06-24 09:20:34 +0200

    extended logging functionality to mention if no deletions occured

--- git-clean-simple/git_clean.sh
+++ git-clean-simple/git_clean.sh
@@ -2,22 +2,42 @@
 
 #TODO flags für logs oder nicht
 #TODO flags für quiet
-#TODO filepath mit datum
+#TODO dry run functionality with logging
 
-filepath="../$(date +%d-%m-%y)_clean-log.txt"
+function delete {
+    output=$(git clean -x -f);
+    if [ "$output" != "" ]; then
+        if [ "$1" = false ]; then
+            echo "TIME: $(date +%H-%M-%S)" >> ".$2";
+            didSomething=true;
+        fi
+        if [ "$3" = true ]; then
+            echo "$output" >> ".$2";
+        fi
+        if [ "$4" = false ]; then
+            echo "$output";
+        fi
+    fi
+}
+
+function checkForNoDeletions {
+    if [ "$1" = false ]; then
+        if [ "$3" = true ]; then
+            echo "TIME: $(date +%H-%M-%S)" >> "$2";
+            echo "No deletions occured" >> "$2";
+        fi
+        if [ "$4" = false ]; then
+            echo "No files were deleted!"
+        fi
+    fi
+}
+
+filepath="./$(date +%d-%m-%y)_clean-log.txt"
 didSomething=false
+shouldLog=true
+isQuiet=false
 for DIR in */; do
 cd $DIR 
-output=$(git clean -x -f)
-if [ "$output" != "" ]; then
-    if [ "$didSomething" = false ]; then
-        echo "TIME: $(date +%H-%M-%S)" >> "${filepath}"
-        didSomething=true
-    fi
-    echo "$output" >> "${filepath}"
-fi 
+    delete $didSomething $filepath $shouldLog $isQuiet
 cd ..; done
-
-if [ "$didSomething" = false ]; then
-    echo "No files were deleted!"
-fi
+checkForNoDeletions $didSomething $filepath $shouldLog $isQuiet
