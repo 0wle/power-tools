6a9d6be1935f070ac6b739c8d056bb4faf545b8d
hagenahm <michel.hagenah@gmail.com>
2022-06-29 12:54:07 +0200

    fixed dry run, added repository path as prefix in outputs

--- git-clean-simple/git-clean.sh
+++ git-clean-simple/git-clean.sh
@@ -18,10 +18,12 @@ function delete {
             if [ "$1" = false ]; then
             echo "TIME: $(date +%H-%M-%S)" >> "$2";
             fi
-            echo "$5$output" >> "$2";
+            echo $(pwd) >> "$2";
+            echo "$output" >> "$2";
         fi
         if [ "$4" = false ]; then
-            echo "$5$output";
+            echo $(pwd)
+            echo "$output";
         fi
         didSomething=true;
     fi
@@ -31,18 +33,32 @@ function checkForNoDeletions {
     if [ "$1" = false ]; then
         if [ "$3" = true ]; then
             echo "TIME: $(date +%H-%M-%S)" >> "$2";
+            if [ "$5" = false ]; then
             echo "No deletions occured" >> "$2";
+            else
+            echo "No deletions would have occured" >> "$2";
+            fi
         fi
         if [ "$4" = false ]; then
-            echo "No files were deleted!"
+            echo "No files were deleted!";
         fi
     fi
 }
 
 function runDelete {
     if [ -d "./.git/" ]; then
-        delete $1 $2 $3 $4
+        delete $1 $2 $3 $4 $5 $6
     fi
+}
+
+function deleteFromSubDirectories {
+for DIR in */; do 
+cd $DIR
+    if [ $7 = true ]; then
+        deleteFromSubDirectories $1 $2 $3 $4 $DIR $6 $7
+    fi
+    runDelete $1 $2 $3 $4 $5 $6;
+cd ..; done
 }
 
 shouldLog=false;
@@ -50,6 +66,7 @@ filepathLog=$(pwd);
 filepathStart=$(pwd);
 isQuiet=false;
 isDryRun=false;
+isRecursive=false;
 while getopts 'lL:qnrd:' OPTION; do
     case "$OPTION" in
         l)
@@ -66,31 +83,28 @@ while getopts 'lL:qnrd:' OPTION; do
             isDryRun=true
             ;;
         r)
-            echo "Not implemented"
+            isRecursive=true
             ;;
         d)
             filepathStart=$(realpath "$OPTARG");
             ;;
         ?)
-            echo "git_clean [-l | -L directory] [-q] [-t] [-r] [-d]"
-            echo "l     log will be dumped in current directory"
-            echo "L     log will be dumped in specified directory"
-            echo "q     quiet mode"
-            echo "n     dry run without any actual deletions"
-            echo "r     recursively iterate through every sub directory"
-            echo "d     specify the starting directory"
-            exit 1
+            echo "git_clean [-l | -L directory] [-q] [-t] [-r] [-d]";
+            echo "l     log will be dumped in current directory";
+            echo "L     log will be dumped in specified directory";
+            echo "q     quiet mode";
+            echo "n     dry run without any actual deletions";
+            echo "r     recursively iterate through every sub directory";
+            echo "d     specify the starting directory";
+            exit 1;
             ;;
     esac
 done
 shift "$(($OPTIND -1))"
 
-cd $filepathStart
-filepathLog="$filepathLog/$(date +%d-%m-%y)_clean-log.txt"
-didSomething=false
-for DIR in */; do
-runDelete $didSomething $filepathLog $shouldLog $isQuiet $DIR
-cd $DIR
-    runDelete $didSomething $filepathLog $shouldLog $isQuiet $DIR
-cd ..; done
-checkForNoDeletions $didSomething $filepathLog $shouldLog $isQuiet
+cd $filepathStart;
+filepathLog="$filepathLog/$(date +%d-%m-%y)_clean-log.txt";
+didSomething=false;
+runDelete $didSomething $filepathLog $shouldLog $isQuiet $filepathStart $isDryRun;
+deleteFromSubDirectories $didSomething $filepathLog $shouldLog $isQuiet $filepathStart $isDryRun $isRecursive;
+checkForNoDeletions $didSomething $filepathLog $shouldLog $isQuiet $isDryRun;
