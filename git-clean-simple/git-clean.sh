dbc7b22ba36df92714e83930a3287a8aaf28bb3d
hagenahm <michel.hagenah@gmail.com>
2022-11-23 11:59:06 +0100

    added test if git is installed

--- git-clean-simple/git-clean.sh
+++ git-clean-simple/git-clean.sh
@@ -105,6 +105,11 @@ while getopts 'lL:qnrdh:' OPTION; do
 done
 shift "$(($OPTIND -1))"
 
+if ! [ -x "$(command -v git)" ]; then
+  echo 'Error: git is not installed.' >&2
+  exit 1
+fi
+
 cd $filepathStart;
 filepathLog="$filepathLog/$(date +%d-%m-%y)_clean-log.txt";
 didSomething=false;
